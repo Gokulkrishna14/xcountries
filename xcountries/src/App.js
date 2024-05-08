9ebeac4b63737a73ece4557b19cf2c9f785817cc
Gokulkrishna14 <nairgokul22@gmail.com>
2024-05-08 21:50:03 +0530

    Mini Buildout done

--- xcountries/src/App.js
+++ xcountries/src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const[countries, setCountries] = useState([]);
+
+  const url="https://restcountries.com/v3.1/all";
+
+  useEffect(()=> {
+    fetch(url).then((response) => response.json()).then((data) => setCountries(data)).catch((error) => console.log(error));;
+  }, [])
+
+  console.log(countries);
+
   return (
     <div className="App">
-      Working
+      {countries.map((country, index) => ( 
+          <div key={index} className="Card">
+          <img src={country.flags.png} alt={country.flags.svg} width={"100px"} height={"100px"} /> 
+          <h3>{country.region}</h3>
+        </div>
+      ))}
     </div>
   );
 }
