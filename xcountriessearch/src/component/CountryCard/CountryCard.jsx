70a0147576a8cc6410ec2eb0e41d4e91043185ad
Gokulkrishna14 <nairgokul22@gmail.com>
2024-05-10 21:01:45 +0530

    test case failure

--- xcountriessearch/src/component/CountryCard/CountryCard.jsx
+++ xcountriessearch/src/component/CountryCard/CountryCard.jsx
@@ -1,13 +1,12 @@
-import "./CountryCard.module.css";
+import styles from "./CountryCard.module.css";
 
-
-const CountryCard = ({ country }) => {
+const CountryCard= ({country, index}) => {
     return (
-        <div className="countryCard">
-            <img src={country.flags.png} alt={country.flags.svg} width={"100px"} height={"100px"} /> 
-            <h2>{country.name.common}</h2>
+        <div className={styles.countryCard}>
+                    <img src={country.flags.png} alt={country.flags.svg} width={"100px"} height={"100px"} /> 
+                    <h2>{country.name.common}</h2>
         </div>
-    );
+    )
 };
 
 export default CountryCard;
